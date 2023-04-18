4589e190a5b4fd0fa372c8773890cb8957c04bc2
Nephthali DJABON <nephthali.djabon@sogeti.com>
2023-04-18 16:46:46 +0200

    separate build image script

--- src/ennva/example/Docker.groovy
+++ src/ennva/example/Docker.groovy
@@ -12,10 +12,16 @@ class Docker implements Serializable {
 
     def buildDockerImage(String host, String imageName) {
         script.echo "Building docker image"
+        script.sh "docker build -t $host/$imageName:${script.VERSION} ."
+    }
+
+    def dockerLogin(String $host = '') {
         script.withCredentials([script.usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-            script.sh "docker build -t $host/$imageName:${script.VERSION} ."
             script.sh "echo $script.PASS | docker login -u $script.USERNAME --password-stdin $host"
-            script.sh "docker push $host/$imageName:${script.VERSION}"
         }
+    }
+
+    def dockerPush(String host = '', String imageName) {
+        script.sh "docker push $host/$imageName:${script.VERSION}"
     }
 }

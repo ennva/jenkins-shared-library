90a0c47e17d62ffdeb9f62010f6904b6008cb5e0
Nephthali DJABON <nephthali.djabon@sogeti.com>
2023-04-18 15:47:00 +0200

    param not recognized

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call(String imageName) {
     echo "Building docker image"
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-        sh('docker build -t $imageName:${VERSION} .')
-        sh('echo $PASS | docker login -u $USERNAME --password-stdin localhost:8085')
-        sh('docker push $imageName:${VERSION}')
+        sh "docker build -t $imageName:${VERSION} ."
+        sh "echo $PASS | docker login -u $USERNAME --password-stdin localhost:8085"
+        sh "docker push $imageName:${VERSION}"
     }
 }

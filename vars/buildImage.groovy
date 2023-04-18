436bce870ed6f129cceb1250103a435a3e01277b
Nephthali DJABON <nephthali.djabon@sogeti.com>
2023-04-18 16:13:19 +0200

    test 2 var

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String imageName) {
+def call(String host, String imageName) {
     echo "Building docker image"
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-        sh "docker build -t $imageName:${VERSION} ."
-        sh "echo $PASS | docker login -u $USERNAME --password-stdin localhost:8085"
-        sh "docker push $imageName:${VERSION}"
+        sh "docker build -t $host/$imageName:${VERSION} ."
+        sh "echo $PASS | docker login -u $USERNAME --password-stdin $host"
+        sh "docker push $host/$imageName:${VERSION}"
     }
 }

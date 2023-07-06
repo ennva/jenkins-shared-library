eb54d8d99f257ee78956877b65cb88d4d2045831
Ennvaweb <35902574+ennva@users.noreply.github.com>
2023-07-06 10:47:21 -0400

    checking branch if not multibranch pipeline

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "Building the application for branch $BRANCH_NAME"
-    sh 'mvn package'
+    if (env.BRANCH_NAME) {
+        echo "Building the application for branch $BRANCH_NAME"
+    } else {
+        echo "Building the application ..."
+    }
+    
+    sh 'mvn clean package'
 }

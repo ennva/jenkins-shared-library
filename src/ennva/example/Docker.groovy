1b85ac49eb6abd5d35199a52c01f887b3fb6da3b
Ennvaweb <35902574+ennva@users.noreply.github.com>
2023-07-06 08:38:51 -0400

    Update Docker.groovy for string interpolation injection

--- src/ennva/example/Docker.groovy
+++ src/ennva/example/Docker.groovy
@@ -17,7 +17,7 @@ class Docker implements Serializable {
 
     def dockerLogin(String host = '') {
         script.withCredentials([script.usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-            script.sh "echo $script.PASS | docker login -u $script.USERNAME --password-stdin $host"
+            script.sh 'echo $script.PASS | docker login -u $script.USERNAME --password-stdin $host'
         }
     }
 
